4fcfc05bec82da1559b51f7ea9fb5d302b9e7118
mantassapa <71067404+mantassapa@users.noreply.github.com>
2024-05-23 23:53:33 +0300

    adding simple filter and search

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+import Filter from "./Components/Filter";
 import Login from "./Components/Login";
 import Navigation from "./Components/Navigation";
 import Registration from "./Components/Registration";
 
 function App() {
+  const [filterSelectValue, setfilterSelectValue] = useState("all")
+  const [filterInputValue, setfilterInputValue] = useState("")
+
+
   return (
     <>
       {/* <Registration /> */}
       <Navigation />
+      <Filter filterInputValue={filterInputValue} setfilterInputValue={setfilterInputValue} setfilterSelectValue={setfilterSelectValue}/>
     </>
   );
 }
