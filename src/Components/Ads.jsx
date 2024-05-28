57529a70152e1988ba320c3fe1be54843c2f208b
ugniususc <ugniususc@gmail.com>
2024-05-28 14:40:17 +0300

    Dealing with likes

--- src/Components/Ads.jsx
+++ src/Components/Ads.jsx
@@ -13,13 +13,70 @@ const Ads = ({
   handleAdDelete,
 }) => {
   const [comments, setComments] = useState({});
-  const [click, setClick] = useState(false);
+  const [likedAds, setLikedAds] = useState({});
 
-  function handleLike() {
-    setClick((prev) => !prev);
-  }
+  useEffect(() => {
+    // Load liked ads from local storage
+    const storedLikedAds = JSON.parse(localStorage.getItem("likedAds"));
+    if (storedLikedAds) {
+      setLikedAds(storedLikedAds);
+    }
+  }, []);
+
+  const handleLike = async (adId) => {
+    const newLikedAds = { ...likedAds };
+
+    if (newLikedAds[adId]) {
+      delete newLikedAds[adId];
+    } else {
+      newLikedAds[adId] = true;
+    }
+
+    setLikedAds(newLikedAds);
+    localStorage.setItem("likedAds", JSON.stringify(newLikedAds));
+
+    try {
+      const userData = JSON.parse(localStorage.getItem("userData"));
+      const token = userData?.token;
+
+      if (!token) {
+        throw new Error("No token");
+      }
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      await axios.post(
+        `http://localhost:5000/api/ads/${adId}/like`,
+        {},
+        config
+      );
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  // console.log(key);
+
+  // async function handleLikes() {
+  //   try {
+  //     const res = await axios.post(
+  //       `http://localhost:5000/api/ads/6655a8b6fb8d4d7987ac9eac/like`
+  //     );
+
+  //     console.log(res.data);
+  //   } catch (error) {
+  //     if (error.response && error.response.status === 400) {
+  //       console.log("User exists");
+  //     }
+  //   }
+  // }
 
   const userData = JSON.parse(localStorage.getItem("userData")) || {};
+  const userId = userData._id;
   let adsCopy = [...ads];
 
   // sorting
@@ -41,55 +98,67 @@ const Ads = ({
 
   const handleAdUpdate = (id) => {};
 
+  console.log(userData);
+
   return (
     <div className={styles.container}>
       <h2>{filterSelectValue} Ads</h2>
       <div className={styles.adContainer}>
         {filteredAds.map((ad) => (
-          <div key={ad._id} className={styles.singleAd}>
-            {/* <div className={styles.imgContainer}> */}
-            {ad.images.map((image) => (
-              <img key={image} src={image} className={styles.adImage} />
-            ))}
-            <i onClick={handleLike}>
-              {click ? (
-                <FaHeart className={styles.liked} />
-              ) : (
-                <FaRegHeart className={styles.unliked} />
-              )}
-            </i>
-            {/* </div> */}
-            <div className={styles.adContent}>
-              <div>
-                <h2>{ad.name}</h2>
-                <div className={styles.manageAdBtns}>
-                  {userData._id === ad.user._id && (
-                    <Button type="edit" onClick={() => handleAdUpdate(ad._id)}>
-                      &#9998;
-                    </Button>
+          <>
+            {/* {console.log(ad)} */}
+            <div key={ad._id} className={styles.singleAd}>
+              {/* <div className={styles.imgContainer}> */}
+              {ad.images.map((image) => (
+                <img key={image} src={image} className={styles.adImage} />
+              ))}
+              {userData.token ? (
+                <i onClick={() => handleLike(ad._id)}>
+                  {likedAds[ad._id] && likedAds[ad._id][userId] ? (
+                    <FaHeart className={styles.liked} />
+                  ) : (
+                    <FaRegHeart className={styles.unliked} />
                   )}
-                  {(userData._id === ad.user._id ||
-                    userData.role === "admin") && (
-                    <Button
-                      type="delete"
-                      onClick={() => handleAdDelete(ad._id)}
-                    >
-                      &times;
-                    </Button>
-                  )}
-                </div>
-                <p className={styles.price}>{ad.price}&euro;</p>
-                <p className={styles.category}>{ad.category.name}</p>
+                  {/* {key === ad._id ? <FaHeart /> : <FaRegHeart />} */}
+                </i>
+              ) : null}
 
-                <p>{ad.description}</p>
+              {/* </div> */}
+              <div className={styles.adContent}>
+                <div>
+                  <h2>{ad.name}</h2>
+                  <div className={styles.manageAdBtns}>
+                    {userData._id === ad.user._id && (
+                      <Button
+                        type="edit"
+                        onClick={() => handleAdUpdate(ad._id)}
+                      >
+                        &#9998;
+                      </Button>
+                    )}
+                    {(userData._id === ad.user._id ||
+                      userData.role === "admin") && (
+                      <Button
+                        type="delete"
+                        onClick={() => handleAdDelete(ad._id)}
+                      >
+                        &times;
+                      </Button>
+                    )}
+                  </div>
+                  <p className={styles.price}>{ad.price}&euro;</p>
+                  <p className={styles.category}>{ad.category.name}</p>
+
+                  <p>{ad.description}</p>
+                </div>
+                <Comments
+                  adId={ad._id}
+                  comments={comments[ad._id]}
+                  setComments={setComments}
+                />
               </div>
-              <Comments
-                adId={ad._id}
-                comments={comments[ad._id]}
-                setComments={setComments}
-              />
             </div>
-          </div>
+          </>
         ))}
       </div>
     </div>
--- src/Components/UserAds.jsx
+++ src/Components/UserAds.jsx
@@ -1,13 +1,13 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
-import Button from './Button';
-import Comments from './Comments';
-import styles from './UserAds.module.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import Button from "./Button";
+import Comments from "./Comments";
+import styles from "./UserAds.module.css";
 
-const BASE_URL = 'http://localhost:5000/api/ads';
+const BASE_URL = "http://localhost:5000/api/ads";
 
-const UserAds = ({ comments, setComments, handleAdDelete }) => {
+const UserAds = ({ setComments, handleAdDelete }) => {
   const [ads, setAds] = useState([]);
   const [click, setClick] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
@@ -16,11 +16,11 @@ const UserAds = ({ comments, setComments, handleAdDelete }) => {
     const fetchAds = async () => {
       setIsLoading(true);
       try {
-        const userData = JSON.parse(localStorage.getItem('userData'));
+        const userData = JSON.parse(localStorage.getItem("userData"));
         const token = userData?.token;
 
         if (!token) {
-          throw new Error('No token');
+          throw new Error("No token");
         }
 
         const config = {
@@ -45,7 +45,7 @@ const UserAds = ({ comments, setComments, handleAdDelete }) => {
 
   const handleAdUpdate = (id) => {};
 
-  const userData = JSON.parse(localStorage.getItem('userData')) || {};
+  const userData = JSON.parse(localStorage.getItem("userData")) || {};
   console.log(userData);
 
   return (
@@ -69,7 +69,7 @@ const UserAds = ({ comments, setComments, handleAdDelete }) => {
                     </Button>
                   )}
                   {(userData._id === ad.user._id ||
-                    userData.role === 'admin') && (
+                    userData.role === "admin") && (
                     <Button
                       type="delete"
                       onClick={() => handleAdDelete(ad._id)}
@@ -84,11 +84,9 @@ const UserAds = ({ comments, setComments, handleAdDelete }) => {
                 <p>{ad.description}</p>
               </div>
               <div className={styles.overallNum}>
-                <Comments adId={ad._id} setComments={setComments} />
-
                 <p>
                   Likes <FaHeart className={styles.liked} />:
-                  {/* <span> {ad.likes.length}</span> */}
+                  <span> {ad.likes.length}</span>
                 </p>
                 <p>
                   Comments: <span>{ad.comments.length}</span>
